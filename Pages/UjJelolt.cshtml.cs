d6ff95e2652da9c3238ba1ec0a7fc8fb361b1e71
hunor64 <nhunor06@gmail.com>
2024-10-03 09:03:41 +0200

    Added partok

---
+++ Pages/UjJelolt.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Valasztasok.Models;
+
+namespace Valasztasok.Pages
+{
+    public class UjJeloltModel : PageModel
+    {
+        private readonly Valasztasok.Models.ValasztasDbContext _context;
+
+        public UjJeloltModel(Valasztasok.Models.ValasztasDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["PartRovidNev"] = new SelectList(_context.Partok, "RovidNev", "RovidNev");
+            return Page();
+        }
+
+        [BindProperty]
+        public Jelolt Jelolt { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Jeloltek.Add(Jelolt);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
